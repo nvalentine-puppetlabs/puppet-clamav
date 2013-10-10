9a9a3d99f129000d12af7ff1fe8ce068c88e82e3
Nathan Valentine <nathan@puppetlabs.com>
2013-10-10 23:24:35 +0000

    add install options to params

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,13 @@
 class clamav(
   $cron_options = hiera('clamav::cron_options', {}),
-  $database_mirror = hiera('clamav::database_mirror', 'db.us.clamav.net')
+  $database_mirror = hiera('clamav::database_mirror', 'db.us.clamav.net'),
+  $install_options = hiera('clamav::install_options', '')
 ) inherits clamav::params {
 
-  package { $package: ensure => installed, }
+  package { $package: 
+    ensure => installed,
+    install_options => $install_options,
+  }
 
   file { $config:
     ensure => file,
