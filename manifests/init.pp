5f89844236fea26872bddbdc4b3cd9f8cff42026
Nathan Valentine <nathan@puppetlabs.com>
2013-10-11 11:47:23 -0700

    adjust excludes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class clamav(
 
   $scan_command = hiera('clamav::scan_command', '/usr/bin/clamscan -r -i /home /var'),
-  $exclude = hiera('clamav::scan', ['/sys', '/proc']), 
+  $exclude = hiera('clamav::scan', ['^/sys', '^/proc']), 
   $cron_timespec = hiera('clamav::cron_timespec', '00 1 * * *'),
   $cron_mailto = hiera('clamav::cron_mailto', 'root'),
   $database_mirror = hiera('clamav::database_mirror', 'db.us.clamav.net')
@@ -21,7 +21,7 @@ class clamav(
     require => Package[$package],
   }
 
-  $excludes = inline_template("<% @exclude.each do |ex| -%>--exclude=<%= ex -%><% end -%>")
+  $excludes = inline_template("<% @exclude.each do |ex| -%>--exclude=<%= ex -%> <% end -%>")
   $scan_command_real = "${scan_command} ${excludes}"
 
   file { '/etc/cron.d/clamscan':
