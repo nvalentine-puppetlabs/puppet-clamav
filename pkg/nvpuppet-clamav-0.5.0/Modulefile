9a82d13d555f1d84ae0b930188a45f30cdce5498
Nathan Valentine <nathan@puppetlabs.com>
2013-10-11 11:58:40 -0700

    Forge-ify

---
+++ pkg/nvpuppet-clamav-0.5.0/Modulefile
@@ -0,0 +1,11 @@
+name    'nvpuppet-clamav'
+version '0.5.0'
+source 'git://github.com/nvalentine-puppetlabs/puppet-clamav'
+author 'Nathan Valentine - nathan@puppetlabs.com|nrvale0@gmail.com'
+license 'Apache Version 2.0'
+summary 'ClamAV module for RedHat'
+description 'ClamAV module for RedHat'
+project_page 'http://github.com/nvalentine-puppetlabs/puppet-clamav'
+
+## Add dependencies, if any:
+dependency 'puppetlabs/stdlib', '4.1.0'
---
+++ pkg/nvpuppet-clamav-0.5.0/manifests/init.pp
@@ -0,0 +1,31 @@
+class clamav(
+
+  $scan_command = hiera('clamav::scan_command', '/usr/bin/clamscan -r -i /home /var'),
+  $exclude = hiera('clamav::scan', ['^/sys', '^/proc']), 
+  $cron_timespec = hiera('clamav::cron_timespec', '00 1 * * *'),
+  $cron_mailto = hiera('clamav::cron_mailto', 'root'),
+  $database_mirror = hiera('clamav::database_mirror', 'db.us.clamav.net')
+
+) inherits clamav::params {
+
+  validate_string($scan_command)
+  validate_array($exclude)
+  validate_string($cron_timespec)
+  validate_string($cron_mailto)
+  validate_string($database_mirror)
+  
+  package { $package: ensure => installed, }
+
+  file { $config: ensure => file, mode => '0644', owner => 'root', group => 'root',
+    content => template("${module_name}/freshclam.conf.erb"),
+    require => Package[$package],
+  }
+
+  $excludes = inline_template("<% @exclude.each do |ex| -%>--exclude=<%= ex -%> <% end -%>")
+  $scan_command_real = "${scan_command} ${excludes}"
+
+  file { '/etc/cron.d/clamscan':
+    ensure => file, mode => '0644', owner => 'root', group => 'root',
+    content => template("${module_name}/cron.erb"),
+  }
+}
