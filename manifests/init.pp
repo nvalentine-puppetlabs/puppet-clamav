b05901dec1ba34120a54b49eed4a29b31ed4deee
Nathan Valentine <nathan@puppetlabs.com>
2013-10-11 11:43:35 -0700

    generate cron

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,31 @@
 class clamav(
-  $cron_options = hiera('clamav::cron_options', {}),
-  $database_mirror = hiera('clamav::database_mirror', 'db.us.clamav.net'),
-  $install_options = hiera('clamav::install_options', {})
+
+  $scan_command = hiera('clamav::scan_command', '/usr/bin/clamscan -r -i /home /var'),
+  $exclude = hiera('clamav::scan', ['/sys', '/proc']), 
+  $cron_timespec = hiera('clamav::cron_timespec', '00 1 * * *'),
+  $cron_mailto = hier('clamav::cron_mailto', 'root'),
+  $database_mirror = hiera('clamav::database_mirror', 'db.us.clamav.net')
+
 ) inherits clamav::params {
 
-  package { $package: 
-    ensure => installed,
-    install_options => $install_options,
-  }
+  validate_string($scan_command)
+  validate_array($exclude)
+  validate_string($cron_timespec)
+  validate_string($cron_mailto)
+  validate_string($database_mirror)
+  
+  package { $package: ensure => installed, }
 
-  file { $config:
-    ensure => file,
-    mode => '0644',
-    owner => 'root',
-    group => 'root',
+  file { $config: ensure => file, mode => '0644', owner => 'root', group => 'root',
     content => template("${module_name}/freshclam.conf.erb"),
     require => Package[$package],
   }
 
-  if {} != $cron_options {
-    validate_hash($cron_options)
-    $cron_defaults = { user => 'root', ensure => 'present', }
-    create_resources('cron', $cron_options, $cron_defaults)
+  $excludes = inline_template("<% @exclude.each do |ex| -%>--exclude=<%= ex -%><% end -%>")
+  $scan_command_real = "${scan_command} ${excludes}"
+
+  file { '/etc/cron.d/clamscan':
+    ensure => file, mode => '0644', owner => 'root', group => 'root',
+    content => template("${module_name}/cron.erb"),
   }
 }
